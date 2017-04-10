c0a43e8fe95d05c83ef8b5a215f4de594316ed43
Cristhian Won <cristhian.won@gmail.com>
2017-04-10 01:46:03 -0300

    comment execution script 8 and 9

--- script8/mainFile.js
+++ script8/mainFile.js
@@ -1,3 +1,10 @@
+/*
+Execute in terminal:
+
+node mainFile.js
+
+*/
+
 require('./module1');
 require('./module2');
 require('./module3');
--- script9/mainFile.js
+++ script9/mainFile.js
@@ -1,2 +1,9 @@
+/*
+Execute in terminal:
+
+node mainFile.js
+
+*/
+
 require('./module1');
 require('./module2');
